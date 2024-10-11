4cf5fa5ee44fdf66a1459e6522a8a13760873c94
laxit-freelancing <lakshit.freelancing@gmail.com>
2024-10-11 23:39:51 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,8 @@
+SQLHOST = "mysql-eaa96b8-happycode-f470.f.aivencloud.com"
+DB = "defaultdb"
+TABLE = 'output_data'
+SQLPASS= "AVNS_uO-qQFsrD0NFuqwx1Vf"
+SQLUSER="avnadmin"
+OUTPUT_IMAGE_DIR = "processed_images"
+CSV_INPUT = 'input.csv'
+CSV_OUTPUT = 'output.csv'
